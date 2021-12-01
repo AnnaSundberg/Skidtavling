3be974357abde01d53ced4cf8f1a75825ebc9cc4
Joakim Gårdhagen <superapa.spanx@gmail.com>
2021-12-01 20:10:34 +0100

    Deltagare Lista .txt intit

--- src/com/ya/joakim/test/UppstallningAvDeltagare.java
+++ src/com/ya/joakim/test/UppstallningAvDeltagare.java
@@ -5,6 +5,11 @@ import com.ya.skidtavling.deltagare.Participants;
 //test Kommentar
 
 public class UppstallningAvDeltagare {
-	Participants[] delagare = new Participants[100];
+	Participants[] deltagare = new Participants[100];
+	
+	Participants deltagare1 = new Participants("1","Gunde",1);
+	
+		
+	
 	}
 
--- src/com/ya/skidtavling/main/Main.java
+++ src/com/ya/skidtavling/main/Main.java
@@ -1,13 +1,24 @@
 package com.ya.skidtavling.main;
 
+
 import com.ya.skidtavling.deltagare.Participants;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Participants deltagare = new Participants("1","Gunde",1);
+		Participants[] deltagareLista = new Participants[100];
 		
-		System.out.println(deltagare);
+		//int plats = 0;
+		for(int i = 0;i<deltagareLista.length;i++) {
+			Participants deltagare = new Participants("1","Gunde",i);
+			deltagareLista[i]= deltagare;
+			//plats++;
+		}
+		for(int i = 0;i<deltagareLista.length;i++) {
+			System.out.println(deltagareLista[i]);
+			
+		}
+		System.out.println("\n\n\n"+deltagareLista[9]);
 
 	}
 
