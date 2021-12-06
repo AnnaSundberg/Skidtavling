dd6732dee9a7edd66c664f7a45ece7ffafe63d66
selaxhans <hans.stal@yaelev.se>
2021-12-06 01:55:07 +0100

    lag till codesnippet menyhantering för inklistring efter behov

---
+++ src/com/ya/Skidtavling/com.ya.skidtavling/src/snippet_meny/package-info.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+/**
+ * @author test
+ *
+ */
+package snippet_meny;
+
+
+
+
+// TODO Auto-generated method stub
+System.out.println("\t\tMENY välj funktion en av nedanstående ange nummer");
+System.out.println("\t\t0. Avsluta");
+System.out.println("\t\t1. TÄVLING");
+System.out.println("\t\t2. DELTAGARE");
+System.out.println("\t\t3. LÄS IN DELTAGARLISTA");
+System.out.println("\t\t4. STARTLISTA");
+System.out.println("\t\t5. STARTA TÄVLING");
+System.out.println("\t\t6. MELLANTIDER REGISTRERA");
+System.out.println("\t\t7. MÅLGÅNG REGISTRERA");
+System.out.println("\t\t8. AVSLUTA PRISUTDELNING.");
+System.out.println("\t\t9. VISA MENYNn");
+System.out.println("\t\t10. UTSKRIFTER");
+System.out.println("\t\t11. XTRAS");
+System.out.println("\t\t12. XTRA 2");
+
+return;
