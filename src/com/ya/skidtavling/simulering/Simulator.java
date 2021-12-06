48508481f7506fb39c20391567178779b6d4f55b
Joakim Gårdhagen <superapa.spanx@gmail.com>
2021-12-06 21:39:41 +0100

    kommentar Idé ish

--- src/com/ya/skidtavling/simulering/Simulator.java
+++ src/com/ya/skidtavling/simulering/Simulator.java
@@ -5,8 +5,10 @@ import java.util.Random;
 public class Simulator {
 	public Simulator() throws InterruptedException {
 		Random rand = new Random();
+		// gissing är att det är typ här man kan kasta in data från deltagare som ska vara med i tävling som 
+		// så skriver ut en simulering nu ä'r det bara en random som gör x antal sjärnor.
 		int outPut = rand.nextInt(100);
-
+		 
 		System.out.println(outPut);
 		PrintStars.PrintStars(outPut);
 
