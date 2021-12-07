d5dc0e0a1b3469a9f1d27fc7240da1796d128bf9
Joakim Gårdhagen <superapa.spanx@gmail.com>
2021-12-07 21:57:50 +0100

    test

--- src/com/ya/skidtavling/tavla/TavlingsTavla.java
+++ src/com/ya/skidtavling/tavla/TavlingsTavla.java
@@ -11,27 +11,27 @@ public class TavlingsTavla {
 	public Participants[] deltagareLista = new Participants[100];
 	Random rand = new Random();
 //	ArrayList<Participants> dl = new ArrayList<Participants>();//kanske skall byta ut arrayen till en LISTA istället.
-	
+
 	public void fill() {
-		
+
 		// Läser in deltagarena från filen
 		try (BufferedReader fReader = new BufferedReader(new FileReader("Deltagare.txt"))) {
 
 			String line;
 			int i = 0;
 			long tid = 0;
-			//ställer upp deltagarna rad för rad
+			// ställer upp deltagarna rad för rad
 			while ((line = fReader.readLine()) != null) {
-				String split[] = line.split(" ");// stringSplitar raden 
-				String fName = split[0];// sparar förnamn 
+				String split[] = line.split(" ");// stringSplitar raden
+				String fName = split[0];// sparar förnamn
 				String lName = split[1];// sparar efternamn
 				int randNum = rand.nextInt(100);
-				
-				Participants newDeltag = new Participants(randNum, fName, lName, i + 1,tid,0);
-										//skapar objektet som innan men nu kopplat med Personklassen.					
+
+				Participants newDeltag = new Participants(randNum, fName, lName, i + 1, tid, 0);
+				// skapar objektet som innan men nu kopplat med Personklassen.
 				deltagareLista[i] = newDeltag;
 				i++;
-				tid +=15;
+				tid += 15;
 
 			}
 		} catch (Exception e) {
@@ -46,22 +46,25 @@ public class TavlingsTavla {
 			System.out.println(deltagareLista[k]);
 		}
 	}
+
 	public Participants printOne(int x) {// den Här verkar inte fungera*note Uppdaterad
 		return deltagareLista[x];
-		}
+	}
+
 	public void Race() { // ett sätt jag fick ihop simuleringen funkar lite sisådär
 		boolean winner = false;
-		do{
-		for (int i = 0; i < deltagareLista.length; i++) {
-		int distancedTraveled = rand.nextInt(10);
-			deltagareLista[i].setDistance(deltagareLista[i].getDistance()+distancedTraveled);
-			System.out.println(deltagareLista[i]);
-			if(deltagareLista[i].getDistance()==100) {
-				System.out.println("\n\nVinnaren för tävlingen är "+ deltagareLista[i].getForName()+" "+deltagareLista[i].getLastName().toString());
-				winner = true;
+		do {
+			for (int i = 0; i < deltagareLista.length; i++) {
+				int distancedTraveled = rand.nextInt(10);
+				deltagareLista[i].setDistance(deltagareLista[i].getDistance() + distancedTraveled);
+				System.out.println(deltagareLista[i]);
+				if (deltagareLista[i].getDistance() == 100) {
+					System.out.println("\n\nVinnaren för tävlingen är " + deltagareLista[i].getForName() + " "
+							+ deltagareLista[i].getLastName().toString());
+					winner = true;
+				}
 			}
-		}
-		}while(!winner);
+		} while (!winner);
 	}
 
 }
