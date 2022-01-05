771735a83b828728482f5417390605c1ed570a9d
Joakim Gårdhagen <superapa.spanx@gmail.com>
2022-01-05 22:33:30 +0100

    just a comment

--- src/com/ya/skidtavling/main/Main.java
+++ src/com/ya/skidtavling/main/Main.java
@@ -10,5 +10,12 @@ public class Main {
 		menu.runMenu(); // kallar på runMenu klassen som skriver ut välkommen rutan och sedan valen
 
 	}
-
+/*Det som behövs göras nu är tänka ut en utveckling av gemförande med tiderna 
+ * öm det jag kan tänka mig fungera är att man kanske skall ha en variable som spara start tid och målgångs
+ * tid och vid gemförelsen får vi en offset som vi måste använda oss av vid uträkningen av 
+ * tidsskillnaden på vinnaren och deltagaren i fråga 
+ * min gissning är att det är egentligen det sista som behövs och 
+ * ja sedan kommentera alla kodblock men det kanske går snabbt om vi sitter ihop
+ * Gissar på att det kan bli enklare att få ihop kommentarer när man försöker förklara för varandra :) 
+ * */
 }
