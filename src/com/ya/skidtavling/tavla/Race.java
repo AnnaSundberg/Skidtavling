c1c4600e96c59bb88962bab8ac016ce4b1deb3c3
Joakim Gårdhagen <superapa.spanx@gmail.com>
2022-01-06 17:28:02 +0100

    Kommentar och CleanUp

--- src/com/ya/skidtavling/tavla/Race.java
+++ src/com/ya/skidtavling/tavla/Race.java
@@ -4,7 +4,13 @@ import java.util.Random;
 
 import com.ya.skidtavling.participants.Participants;
 import com.ya.skidtavling.time.Time;
-
+/*Race Klassen fungerar så att man kasta in listan av Deltagare och loopar 
+ * igenom den med en forEach, för varje deltagare så ändrar han 
+ * på dess diverse värden som Distanc och tid
+ * När deltagaren har nått 50% av loppet så sparas 
+ * den in i en MellantidsLista
+ * för att sedan fortsätta till mål och då sparas deltagaren i en ResultatLista  
+ * */
 public class Race {// Flyttad till egen klass för att snygga till det hela
 
 	Random rand = new Random();
@@ -23,17 +29,11 @@ public class Race {// Flyttad till egen klass för att snygga till det hela
 
 				if (!p.isNotFinished()) {
 					p.setDistance(p.getDistance() + rand.nextInt(5));
-					/*
-					 * Idé 'är att vi skall använda randen för att generera fram en tid som blir
-					 * kopplad till tid
-					 */
 					p.startTime.setSec(p.startTime.getSec() + plusSec);// man måste ta class metoderna så fungera det :)
 					System.out.println(p.toString());
-					// Här måste tittas över
+					// Har åkaren nått mitten ?
 					if (!p.isPassedMiddle() && p.getDistance() >= trackDistance / 2) {
-						if (!p.isPassedMiddle() && p.getDistance() <= (trackDistance / 2) + 5) {// vet inte om detta gör
-																								// så mkt längre
-							// om längd är 500 ha denna på 250
+						if (!p.isPassedMiddle() && p.getDistance() <= (trackDistance / 2) + 5) {
 							System.out.println("midList Update-----------------------------------------");
 							p.setPlace(midPlace);
 							getMidTimeFromPartic = p.getStartTime().getSec();
@@ -48,13 +48,12 @@ public class Race {// Flyttad till egen klass för att snygga till det hela
 						System.out.println(" ----");
 					} // kan det vara detta som buggar det hela och gör så det krashar ?
 
-					if (p.getDistance() >= trackDistance) {// gissar på att denna är den som styr loppets längd. testa
-															// om det går att göra det längre med 500 istället
+					if (p.getDistance() >= trackDistance) {
 						p.setPlace(finishPlace);
 						System.out.println("\n  " + p.getForName() + " " + p.getLastName() + " Har Gått i mål");
 						finishPlace++;
 						p.setDistance(p.getDistance());
-						compBoard.resultBoard.add(p);
+						compBoard.resultBoard.add(p);// Sparar deltagaren i ResultatListan
 						p.setNotFinished(true);
 
 					}
@@ -77,8 +76,14 @@ public class Race {// Flyttad till egen klass för att snygga till det hela
 	public void midListUpdate(Participants p, CompetitionBoard compBoard, Time midTime) {// kastar nu in mellantiden
 		// gör en ny deltagare och copy n pastear Värderna för att sedan spara in dem i
 		// Mid mellantidsListan
-		Participants midListPar = new Participants(p.getParticipantNumber(), p.getForName(), p.getLastName(),
-				p.getPlace(), p.getStartTime(), p.getDistance(), p.isNotFinished(), p.isPassedMiddle());
+		Participants midListPar = new Participants(p.getParticipantNumber()
+				, p.getForName()
+				, p.getLastName()
+				, p.getPlace()
+				, p.getStartTime()//Den här går inte att ta bort Men i o med midTime Så sparas han rätt.
+				, p.getDistance()
+				, p.isNotFinished()
+				, p.isPassedMiddle());
 		midListPar.setStartTime(midTime);// sätter mellantiden för mellantidslistan
 		compBoard.midTimeList.add(midListPar);// lägger till åkaren i listan för mellantid
 	}
