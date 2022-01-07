05ed488604cdedb5d5a973699d9364b7c93892c9
Joakim Gårdhagen <superapa.spanx@gmail.com>
2022-01-07 18:38:32 +0100

    comments tillagd

--- src/com/ya/skidtavling/tavla/CompetitionBoard.java
+++ src/com/ya/skidtavling/tavla/CompetitionBoard.java
@@ -6,11 +6,14 @@ import com.ya.skidtavling.participants.Participants;
 import com.ya.skidtavling.time.Time;
 
 public class CompetitionBoard {
-
+	
+	// Instanser för listorna som används i programmet
 	public List<Participants> participantsList = new ArrayList<>();
 	public List<Participants> resultBoard = new ArrayList<>();
 	public List<Participants> midTimeList = new ArrayList<>();
 	
+	// Ställer upp listan men med en slumpad metod detta gör att det inte är första namnet som 
+	// skrivs ut , utan ett slumpat namn ,Denna metod anropar även TidsTilldelnings metoden.
 	public void randomizeList() {
 		Collections.shuffle(participantsList);
 		startingTime();
@@ -27,7 +30,9 @@ public class CompetitionBoard {
 		for(Participants p:participantsList) {
 			Time startTime = new Time(twoPm+plusTime);
 			p.setStartTime(startTime);
-			plusTime+=15;
+			plusTime+=15;// lägger på 15 för tilldelning av intervallstart
+			
+			// vi Skiter i detta !! 
 			// behöver någon typ av start och mål tid för att få någon typ av offset 
 			// funderar på om man kanske inte skall ha en till parameter i Participant som 
 			// heter just finnishingTime lr något 
@@ -43,21 +48,22 @@ public class CompetitionBoard {
 		}
 	}
 
-	public void printResultBoard() {
+	public void printResultBoard() {// Skriver ut resultalistan
 
 		for (Participants s : resultBoard) {
 			System.out.println(s);
 		}
 	}
-	public void printMidTimeList() {
+	public void printMidTimeList() { // Skriver ut mellantidslistan
 		for (Participants s : midTimeList) {
 			System.out.println(s);
 		}
 	}
-	public void printWinnerBoard()
+	public void printWinnerBoard()// Skriver ut vinnarna i tävlingen 
 	{
 		try
-		{
+		{// gör en strängar av index 1,3,2 i Resultatlistan Och sparar dessa 3s för och efternamn 
+			// för att sedan skirva utt dessa på skärmen lite snyggt
 		String firstPlaced = resultBoard.get(0).getForName()+" "+resultBoard.get(0).getLastName();
 		String secondPlaced = resultBoard.get(1).getForName()+" "+resultBoard.get(1).getLastName();
 		String thirdPlaced =resultBoard.get(2).getForName()+" "+resultBoard.get(2).getLastName();
